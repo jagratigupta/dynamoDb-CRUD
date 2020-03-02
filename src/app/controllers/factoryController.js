cecb31678831aba538d22eddd09cb468820525b2
amit <naveen.kumar@truringsoftek.com>
2020-03-03 02:38:06 +0530

    changes

--- src/app/controllers/factoryController.js
+++ src/app/controllers/factoryController.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var rs = require('../utilities/responseStructure');
 
 const factory = require('../models/factory')
 
---
+++ src/app/utilities/logger.js
@@ -0,0 +1,34 @@
+const apm = require('elastic-apm-node').start({ 
+    serviceName: 'Mock Test Platform',
+    secretToken: '',
+    // serverUrl: 'http://172.31.4.249:8200'
+    serverUrl: 'http://52.66.251.85:8200'
+  })
+  
+  
+  const winston = require("winston");
+  const wLogger = winston.createLogger();
+  
+    var client = require('./connectElastic');
+    var winston_elasticsearch = require('winston-elasticsearch');
+  
+    function logger(collection="mtp_application_logs") {
+      wLogger.add(new winston.transports.Console());
+     
+      try {
+        return wLogger.add( new winston_elasticsearch({
+          client,
+          index: collection
+        }));
+      } catch (error) {
+        apm?apm.captureError(error):"";
+        // console.log(error)
+        return wLogger.add(error);
+      }
+    }
+    
+  module.exports = {
+    apm: apm,
+    logger: logger
+  
+  }
