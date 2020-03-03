9f28bef44b9d281ff57fb836dab97334ca3022cf
Jagrati <jagratigupta1997@gmail.com>
2020-03-03 13:03:43 +0530

    'changes'

--- src/app/controllers/factoryController.js
+++ src/app/controllers/factoryController.js
@@ -6,29 +6,29 @@ const factory = require('../models/factory')
 
 
 //get api controller of factory
-router.get('/',(req,res,next) => {
+router.get('/read',(req,res,next) => {
     console.log("step-1",req.query);
     factory.read(req,(err,result) =>{
         if(err){
             //res.status(rs.resCodes.error.code).json(rs.errRes(err));
-            res.send(err);
+            res.status(500).send(err)
         }
         else{
-            // res.status(rs.resCodes[req.method].code).json(rs.successObjRes(result));
-            res.status(200).json(result);
+            //res.status(rs.resCodes[req.method].code).json(rs.successRes(result));
+            res.status(200).send(JSON.stringify(result))
         }
     })
 })
 
-router.put('/write',(req,res,next) => {
+router.post('/write',(req,res,next) => {
     factory.write(req,(err,result) =>{
         if(err){
             //res.status(rs.resCodes.error.code).json(rs.errRes(err));
-            res.send(err);
+            res.status(500).send(err)
         }
         else{
-            // res.status(rs.resCodes[req.method].code).json(rs.successObjRes(result));
-            res.status(200).json(result);
+            //res.status(rs.resCodes[req.method].code).json(rs.successObjRes(result));
+            res.status(201).send(JSON.stringify(result))
         }
     })
 })
--- src/app/models/factory.js
+++ src/app/models/factory.js
@@ -9,7 +9,7 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 /////////////////////Read an Item///////////////////////
 function read(req,callback){
     try {
-        var table = "factory";
+        var table = "factory"
         var params = {
             TableName: table,
             Key: {
@@ -20,10 +20,10 @@ function read(req,callback){
 
         docClient.get(params, (err, data) => {
             if (err) {
-                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-                return callback(error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2)))
+                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2))
+                return callback(err)
             } else {
-                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+                console.log("GetItem succeeded:", JSON.stringify(data, null, 2))
                 return callback(null,data)
             }
         });
@@ -36,11 +36,12 @@ function write(req,callback) {
     try {
         console.log("Importing factory data into DynamoDB. Please wait.");
         var created_at = Date.now();
+        console.log("timeStamp",created_at);
         var params = {
             TableName: "factory",
             Item: {
-                "factory_id": req.factory_id,
-                "location": req.location,
+                "factory_id": req.body.factory_id,
+                "location": req.body.location,
                 "created_at": created_at
             }
         };
@@ -55,13 +56,80 @@ function write(req,callback) {
             }
         });
     } catch (error) {
-        callback(error)
+        return callback(error)
     }
     
+}
+
+
+function update(req,callback){
+    try {
+        var table = "factory";
+        var params = {
+            TableName:table,
+            Key:{
+                "year": year,
+                "title": title
+            },
+            // UpdateExpression: "set info.rating = :r, info.plot=:p, info.actors=:a",
+            // ExpressionAttributeValues:{
+            //     ":r":5.5,
+            //     ":p":"Everything happens all at once.",
+            //     ":a":["Larry", "Moe", "Curly"]
+            // },
+            ReturnValues:"UPDATED_NEW"
+        };
+        
+        console.log("Updating the item...");
+        docClient.update(params, function(err, data) {
+            if (err) {
+                console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+            } else {
+                console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
+            }
+        });
+    } catch (error) {
+        return callback(error)
+    }
+}
+
+
+/////////////////////////delete item///////////////////////////
+function del(obj,callback) {
+    try {
+        var table ='factory'
+
+        var params = {
+            TableName: table,
+            Key: {
+                "factory_id": req.body.factory_id,
+                "location": req.body.location
+            },
+           // ConditionExpression: "",
+            // ExpressionAttributeValues: {
+            //     ":val": 50
+            // }
+        };
+
+        console.log("Attempting a conditional delete...");
+        docClient.delete(params, function (err, data) {
+            if (err) {
+                console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+                callback(error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2)))
+            } else {
+                console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
+            }
+        });
+
+    } catch (error) {
+        callback(error)
+    }
+
 }
 
 
 module.exports = {
   read : read,  
   write : write,
+  del : del
 }
